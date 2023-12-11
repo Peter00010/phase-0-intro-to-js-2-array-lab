f83497f859238bcd0b57337e0792f770a14e4b70
Peter00010 <mwongela.oboe@gmail.com>
2023-12-11 09:06:32 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats);
+
+function destructivelyAppendCat(){
+cats.push("Ralph")
+console.log(cats);
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    console.log(cats);
+}
+
+function destructivelyPrependCat(){
+    cats.pop()
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.shift()
+    console.log(cats);
+}
